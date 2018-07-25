34f890ae4ccd70e2653f6c61f75ead7000de40d4
jlongmire02 <jlongmire02@gmail.com>
2018-07-25 15:13:10 +0000

    automatically backed up by learn

---
+++ twosteps.rb
@@ -0,0 +1,39 @@
+def first_steps
+  loop do 
+    puts "Right foot back"
+    sleep(0.5)
+    puts "Left foot back"
+    sleep(0.5)
+    puts "Right foot back"
+    sleep(0.5)
+    puts "Stop"
+    sleep(1)
+  end
+end
+
+def a_few_more_steps
+  loop do 
+    puts "Right foot back"
+sleep(0.5)
+puts "Left foot back"
+sleep(0.5)
+puts "Right foot back"
+sleep(0.5)
+puts "Stop"
+sleep(1)
+puts "Right foot steps right and back"
+sleep(0.5)
+puts "Left foot crosses over right"
+sleep(0.5)
+puts "Right foot steps right"
+sleep(0.5)
+puts "Turn"
+sleep(1)
+end
+end
+
+def how_many_steps?
+  steps = 0 
+end
+end
+
